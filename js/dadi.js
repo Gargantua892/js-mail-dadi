b271c3b72dfea00a38abba3a932793f885d7e708
gargantua892 <94129392+And18892@users.noreply.github.com>
2023-05-04 04:01:00 +0200

    miglioramento grafica e aggiunta gioco dadi

--- js/dadi.js
+++ js/dadi.js
@@ -2,20 +2,34 @@
 let userDice;
 let computerDice;
 
+const userHtml = document.querySelector("#dado-user>h5");
+const computerHtml = document.querySelector("#dado-computer>h5");
+const winner = document.querySelector("#risultato>p");
 
 
 // Generazione random numeri
-userDice = Math.floor((Math.random() * 6) + 1);
+userDice = Math.floor((Math.random() * 6) + 1)
 computerDice = Math.floor((Math.random() * 6) + 1);
 
-console.log(userDice);
-console.log(computerDice);
+// console.log(userDice);
+// console.log(computerDice);
 
-// stabilire chi vince con condizioni
+//aggiunta event listner al bottone per iniziare il gioco
+document.getElementById("btn-dadi").addEventListener("click", function(){
+    // btn-dadi
+    userHtml.innerHTML = userDice;
+    computerHtml.innerHTML = computerDice;
+
+    // stabilire chi vince con condizioni
 if(userDice < computerDice){
     console.log("Ha vinto il pc");
+    winner.innerHTML = "Ha vinto il pc";
 } else if (userDice > computerDice){
     console.log("Ha vinto l'utente");
+    winner.innerHTML = "Ha vinto il giocatore 1";
 }else if (userDice === computerDice){ 
     console.log("pareggio");
+    winner.innerHTML = "Pareggio!";
 }
+});
+
